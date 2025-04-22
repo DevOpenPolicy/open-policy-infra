4d93102beb64a7963fcfd38195b1a4f7ec8f4e32
Wisdom Okechukwu <thewisdomokechukwu@gmail.com>
2025-04-22 21:49:56 +0100

    fix for upload

--- routes/console.php
+++ routes/console.php
@@ -13,24 +13,23 @@ Artisan::command('inspire', function () {
 
 
 Artisan::command('generate:data', function () {
-    // $this->comment('started');
-    // GenerateContentClass::generateMP();
-    // GenerateContentClass::generateBill();
-    // $this->comment('completed');
-    Politicians::truncate();
+    $this->comment('started');
+    GenerateContentClass::generateMP();
+    GenerateContentClass::generateBill();
+    $this->comment('completed');
 })->purpose('Display an inspiring quote')->hourly();
 
 Artisan::command('upload:data', function () {
     $tables = ['politicians'];
     foreach ($tables as $table) {
-        $chunkSize = 500;
+        $chunkSize = 1000;
         $targetUrl = 'https://open-policy-backend-drf8ffeeeehhhhd2.canadacentral-01.azurewebsites.net/api/upload-db';
         $this->comment('Phase 1');
 
         DB::table($table)->orderBy('id')->chunk($chunkSize, function ($rows) use ($table, $targetUrl) {
             $this->comment('Phasing');
-            $dataArray = $rows->map(fn ($row) => (array) $row)->toArray();
-
+            $dataArray = $rows->map(fn ($row) => (array) $row)->toArray();  
+$this->comment(count($dataArray));
             Http::post($targetUrl, [
                 'table' => $table,
                 'data' => $dataArray,
