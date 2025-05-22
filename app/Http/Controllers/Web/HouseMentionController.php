93bdda39d5253fd8366458bf218e189e1d2a8827
Wisdom Okechukwu <thewisdomokechukwu@gmail.com>
2025-05-22 03:47:49 +0100

    fix for links

--- app/Http/Controllers/Web/HouseMentionController.php
+++ app/Http/Controllers/Web/HouseMentionController.php
@@ -6,6 +6,7 @@ use App\Helper\OpenParliamentClass;
 use App\Http\Controllers\Controller;
 use App\Models\Bill;
 use App\Models\Politicians;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class HouseMentionController extends Controller
@@ -19,9 +20,13 @@ class HouseMentionController extends Controller
         $params = request('params');
         $politician = request('politician');
 
+
         $politician = explode('-', $politician);
         $politician_slug = $politician[0].'-'.$politician[1];
         $politician_detail = Politicians::where('politician_url',"LIKE", "%".$politician_slug."%")->first();
+        if(!$politician_detail){
+            $politician_slug = Str::title(str_replace('-', ' ', $politician_slug));
+        }
 
         $url = "https://openparliament.ca$params?singlepage=1";
         $data = $this->openParliamentClass->getParliamentConversation($url);
@@ -29,7 +34,7 @@ class HouseMentionController extends Controller
         return response()->json([
             'success' => true,
             'data' => $data,
-            'politician' => $politician_detail->name,
+            'politician' => $politician_detail ? $politician_detail->name : $politician_slug,
             'number' => $politician[2],
         ]);
     }
--- app/Http/Controllers/v1/AppLinkController.php
+++ app/Http/Controllers/v1/AppLinkController.php
@@ -7,67 +7,78 @@ use Illuminate\Http\Request;
 
 class AppLinkController extends Controller
 {
+    const LINK_URL = 'https://app.openpolicy.me/';
+    // const LINK_URL = 'http://localhost:5173/';
     public function activityLink()
     {
         $link = request('link');
 
-        if (array_filter(['bills', 'debate', 'committees'], fn($keyword) => str_contains($link, $keyword))) {
+        if (array_filter(['bills', 'debate', 'committees', 'votes'], fn($keyword) => str_contains($link, $keyword))) {
             return response()->json([
                 'success' => true,
-                'data' => "https://app.openpolicy.me/$link",
+                'data' => self::LINK_URL."$link",
             ]);
         }
 
         return response()->json([
             'success' => true,
-            'data' => "https://openpolicy.me",
+            'data' => self::LINK_URL,
         ]);
     }
 
     public function debateActivityLink()
     {
-        $type = request('type');
-        if ($type === 'debates_this_month') {
-            return response()->json([
-                'success' => true,
-                'data' => 'https://app.openpolicy.me/debates',
-            ]);
-        } elseif ($type === 'debates_past') {
-            return response()->json([
-                'success' => true,
-                'data' => 'https://app.openpolicy.me/debates',
-            ]);
-        } elseif ($type === 'debates_past') {
-            return response()->json([
-                'success' => true,
-                'data' => 'https://app.openpolicy.me/debates',
-            ]);
-        }
-
         return response()->json([
             'success' => true,
-            'data' => 'https://app.openpolicy.me/debates',
+            'data' => self::LINK_URL.'debates',
         ]);
+        // $type = request('type');
+        // if ($type === 'debates_this_month') {
+        //     return response()->json([
+        //         'success' => true,
+        //         'data' => self::LINK_URL.'/debates',
+        //     ]);
+        // } elseif ($type === 'debates_past') {
+        //     return response()->json([
+        //         'success' => true,
+        //         'data' => 'https://app.openpolicy.me/debates',
+        //     ]);
+        // } elseif ($type === 'debates_past') {
+        //     return response()->json([
+        //         'success' => true,
+        //         'data' => 'https://app.openpolicy.me/debates',
+        //     ]);
+        // }
+
+        // return response()->json([
+        //     'success' => true,
+        //     'data' => 'https://app.openpolicy.me/debates',
+        // ]);
     }
 
     public function committeeActivityLink()
     {
-        $type = request('type');
-        if ($type === 'current_committees') {
-            return response()->json([
-                'success' => true,
-                'data' => 'https://app.openpolicy.me/committees',
-            ]);
-        } elseif ($type === 'recent_studies') {
-            return response()->json([
-                'success' => true,
-                'data' => 'https://app.openpolicy.me/committees',
-            ]);
-        } 
-
         return response()->json([
             'success' => true,
-            'data' => 'https://app.openpolicy.me/committees',
+            'data' => self::LINK_URL.'committees',
         ]);
+
+        // $type = request('type');
+        // if ($type === 'current_committees') {
+        //     return response()->json([
+        //         'success' => true,
+        //         'data' => 'https://app.openpolicy.me/committees',
+        //     ]);
+        // } elseif ($type === 'recent_studies') {
+        //     return response()->json([
+        //         'success' => true,
+        //         'data' => 'https://app.openpolicy.me/committees',
+        //     ]);
+        // } 
+
+        // return response()->json([
+        //     'success' => true,
+        //     'data' => 'https://app.openpolicy.me/committees',
+        // ]);
     }
 }
